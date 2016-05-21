27db469bf77c0c96e9a50fba22191cec9beea158
Shelley O'Neill <shelley.oneill89@gmail.com>
2016-05-21 22:44:18 +1200

    Added ability to sort browse results by name or price

--- routes/browse.js
+++ routes/browse.js
@@ -38,4 +38,70 @@ router.get('/:category', function (req, res, next) {
 
 });
 
+router.get('/price_asc/:category', function (req, res, next) {
+    var category = req.params.category;
+
+    var query = client.query("SELECT * FROM Items WHERE Categories=$1 ORDER BY Price Asc", [category], function (err, result) {
+
+    });
+    console.log(query);
+
+    var results = [];
+    // Stream results back one row at a time
+    query.on('row', function (row) {
+        var listing = new itemListing(row.itemname, row.listingid, row.images, row.price, row.numitems, row.quantity)
+        results.push(listing);
+    });
+
+    // After all data is returned, close connection and return results
+    query.on('end', function () {
+        res.render('browse', { category: category, results: results });
+    });
+
+});
+
+router.get('/price_desc/:category', function (req, res, next) {
+    var category = req.params.category;
+
+    var query = client.query("SELECT * FROM Items WHERE Categories=$1 ORDER BY Price desc", [category], function (err, result) {
+
+    });
+    console.log(query);
+
+    var results = [];
+    // Stream results back one row at a time
+    query.on('row', function (row) {
+        var listing = new itemListing(row.itemname, row.listingid, row.images, row.price, row.numitems, row.quantity)
+        results.push(listing);
+    });
+
+    // After all data is returned, close connection and return results
+    query.on('end', function () {
+        res.render('browse', { category: category, results: results });
+    });
+
+});
+
+router.get('/name/:category', function (req, res, next) {
+    var category = req.params.category;
+
+    var query = client.query("SELECT * FROM Items WHERE Categories=$1 ORDER BY ItemName Asc", [category], function (err, result) {
+
+    });
+    console.log(query);
+
+    var results = [];
+    // Stream results back one row at a time
+    query.on('row', function (row) {
+        var listing = new itemListing(row.itemname, row.listingid, row.images, row.price, row.numitems, row.quantity)
+        results.push(listing);
+    });
+
+    // After all data is returned, close connection and return results
+    query.on('end', function () {
+        res.render('browse', { category: category, results: results });
+    });
+
+});
+
 module.exports = router;
