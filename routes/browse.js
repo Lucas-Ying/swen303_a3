23ca2f725fff5c89dcde70f890d6398295d4cb52
Shelley O'Neill <shelley.oneill89@gmail.com>
2016-05-22 12:18:45 +1200

    Fixed sorting error on borwse page where logo/pics didn't appear

--- routes/browse.js
+++ routes/browse.js
@@ -16,14 +16,21 @@ var itemListing = function(name, id, image, price, qty){
 }
 
 /* GET home page. */
-router.get('/:category', function (req, res, next) {
-    var category = req.params.category;
-
-    var query = client.query("SELECT * FROM Items WHERE Categories=$1", [category], function (err, result) {
-
-    });
-    console.log(query);
-
+router.get('/', function (req, res, next) {
+    var category = req.param('category');
+    var sort = req.param('sort');
+    var query;
+    if (sort === "price_asc") {
+        query = client.query("SELECT * FROM Items WHERE Categories=$1 ORDER BY Price Asc", [category], function (err, result) {});
+    } else if (sort === "price_desc") {
+        query = client.query("SELECT * FROM Items WHERE Categories=$1 ORDER BY Price desc", [category], function (err, result) {});
+        }
+    else if (sort === "name") {
+        query = client.query("SELECT * FROM Items WHERE Categories=$1 ORDER BY ItemName Asc", [category], function (err, result) {});
+    }
+    else{
+        query = client.query("SELECT * FROM Items WHERE Categories=$1", [category], function (err, result) { });
+    }
     var results = [];
     // Stream results back one row at a time
     query.on('row', function(row) {
@@ -38,70 +45,5 @@ router.get('/:category', function (req, res, next) {
 
 });
 
-router.get('/price_asc/:category', function (req, res, next) {
-    var category = req.params.category;
-
-    var query = client.query("SELECT * FROM Items WHERE Categories=$1 ORDER BY Price Asc", [category], function (err, result) {
-
-    });
-    console.log(query);
-
-    var results = [];
-    // Stream results back one row at a time
-    query.on('row', function (row) {
-        var listing = new itemListing(row.itemname, row.listingid, row.images, row.price, row.numitems, row.quantity)
-        results.push(listing);
-    });
-
-    // After all data is returned, close connection and return results
-    query.on('end', function () {
-        res.render('browse', { category: category, results: results });
-    });
-
-});
-
-router.get('/price_desc/:category', function (req, res, next) {
-    var category = req.params.category;
-
-    var query = client.query("SELECT * FROM Items WHERE Categories=$1 ORDER BY Price desc", [category], function (err, result) {
-
-    });
-    console.log(query);
-
-    var results = [];
-    // Stream results back one row at a time
-    query.on('row', function (row) {
-        var listing = new itemListing(row.itemname, row.listingid, row.images, row.price, row.numitems, row.quantity)
-        results.push(listing);
-    });
-
-    // After all data is returned, close connection and return results
-    query.on('end', function () {
-        res.render('browse', { category: category, results: results });
-    });
-
-});
-
-router.get('/name/:category', function (req, res, next) {
-    var category = req.params.category;
-
-    var query = client.query("SELECT * FROM Items WHERE Categories=$1 ORDER BY ItemName Asc", [category], function (err, result) {
-
-    });
-    console.log(query);
-
-    var results = [];
-    // Stream results back one row at a time
-    query.on('row', function (row) {
-        var listing = new itemListing(row.itemname, row.listingid, row.images, row.price, row.numitems, row.quantity)
-        results.push(listing);
-    });
-
-    // After all data is returned, close connection and return results
-    query.on('end', function () {
-        res.render('browse', { category: category, results: results });
-    });
-
-});
 
 module.exports = router;
