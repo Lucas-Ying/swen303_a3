8fa844ed5c24299a8c40be31c7c0ae0089e5a6a1
Lucas-Ying <lucas.yingmm@gmail.com>
2016-05-22 16:35:28 +1200

    create sign up and login authentications

--- routes/index.js
+++ routes/index.js
@@ -25,6 +25,108 @@ router.get('/test_database', function(request, response) {
   });
 });
 
+router.get('/register', function(req, res, next) {
+  res.render('register', { title: 'Join' });
+});
+
+//========================RESTful API for users =====================//
+//get users
+router.get('/get_users', function (req,res){
+  var userName = req.body.name;
+
+  var query = client.query("select * from users");
+  var results =[];
+
+  //error handler for /get_users
+  query.on('error',function(){
+    res.status(500).send('Error, fail to get users: '+userName);
+  });
+
+  //stream results back one row at a time
+  query.on('row',function(row){
+    results.push(row);
+  });
+
+  //After all data is returned, close connection and return results
+  query.on('end',function(){
+    res.json(results);
+    console.log("result: "+results);
+  });
+});
+
+//adding users
+router.post('/add_user', function(req, res){
+  var firstName = req.body.first_name;
+  var lastName = req.body.last_name;
+  var email = req.body.email;
+  var username = req.body.name;
+  var password = req.body.pass;
+
+  var queryid = client.query("SELECT MAX(UserID) FROM Users");
+  var index;
+  var newIndex;
+
+  queryid.on('row', function (row) {
+    index = JSON.parse(row.max);
+    newIndex = (Number)(index + 1);
+  });
+
+  //error handler for /add_user
+  queryid.on('error',function(){
+    res.status(500).send('Error to get user index.');
+  });
+
+  var q = "insert into users (userd, firstname,lastname,userhandle,password,email) values ($1,$2,$3,$4,$5,$6) RETURNING userid,firstname,lastname,userhandle,password,email";
+  var query = client.query(q, [newIndex, firstName, lastName,username,password,email]);
+  var results =[];
+
+  //error handler for /add_user
+  query.on('error',function(){
+    res.status(500).send('Error, fail to add user Name: '+username);
+  });
+
+  //stream results back one row at a time
+  query.on('row',function(row){
+    results.push(row);
+  });
+
+  //after all the data is returned close connection and return result
+  query.on('end',function(){
+    res.json(results);
+    console.log("result: "+results);
+  });
+});
+
+//router.post('/add_user', function (req, res) {
+//  var firstName = req.body.first_name;
+//  var lastName = req.body.last_name;
+//  var email = req.body.email;
+//  var username = req.body.username;
+//  var password = req.body.password;
+//
+//  var query = client.query("SELECT MAX(UserID) FROM Users");
+//  var index;
+//  var newIndex;
+//
+//  query.on('row', function (row) {
+//    index = JSON.parse(row.max);
+//    newIndex = (Number)(index + 1);
+//  });
+//
+//  query.on('end', function () {
+//    client.query('INSERT INTO Users'
+//        + ' (UserID, FirstName, LastName, UserHandle, Password, Email)'
+//        + ' VALUES '
+//        + ' ($1, $2, $3, $4, $5, $6)'
+//        , [newIndex, firstName, lastName, username, password,email]
+//        , function (err, result) {
+//        });
+//    res.render('user_confirmation', { firstName: firstName, username:username, id: newIndex });
+//  });
+//});
+
+//===================================================================//
+
 //========================RESTful API for Product ====================//
 //get items
 router.get('/get_product', function (req,res){
